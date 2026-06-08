705adac403bc206764cd9a8b457d5d4e86f471c3
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-06-08 16:03:48 +0600

    added commit problem-4

---
+++ src/Problem-4.ts
@@ -0,0 +1,10 @@
+// Problem 4: Celsius to Fahrenheit  [Easy]
+// Description: Write a function toFahrenheit(celsius) that converts a Celsius temperature to Fahrenheit.
+// Example:
+// Input: 0   → Output: 32Input: 100 → Output: 212
+// Hint: Formula: (C × 9/5) + 32
+
+const toForenheit=(celsius:number)=>{
+     return ((celsius * 9)/5)+32
+}
+console.log(toForenheit(100))
