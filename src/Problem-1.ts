9420e76e91c31fa00fcc0f5faf17324bf917abd6
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-06-08 15:47:39 +0600

    added commit problem-1

---
+++ src/Problem-1.ts
@@ -0,0 +1,13 @@
+// Problem 1: Swap Two Variables  [Easy]
+// Description: Write a function that swaps the values of two variables without using a third variable.
+// Example:
+// Input: a = 5, b = 10Output: a = 10, b = 5
+// Hint: Try using destructuring or arithmetic operators.
+
+
+let a:number=5;
+let b:number=10;
+
+ [b,a]=[a,b]
+
+ console.log(`a=${a},b=${b}`)
