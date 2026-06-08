a340d912e6dec7c866fd2493d52fa92e3e0c362f
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-06-08 15:55:22 +0600

    added commit problem-2

---
+++ src/Problem-2.ts
@@ -0,0 +1,21 @@
+// Problem 2: Check Even or Odd  [Easy]
+// Description: Write a function isEven(n) that returns true if a number is even, and false if it is odd.
+// Example:
+// Input: 4  → Output: trueInput: 7  → Output: false
+// Hint: Use the modulus (%) operator.
+// method:1
+// const EvenOdd=(num:number):boolean=>{
+//   if(num % 2===0)
+//   {
+//      return true
+//   }else{
+//     return false
+//   }
+// }
+
+// method:2
+const EvenOdd=(num:number):boolean=>{
+ return num %2 ==0
+}
+
+console.log(EvenOdd(8))
