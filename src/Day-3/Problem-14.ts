1a0a0bfb5f4132edd0b785f22314fe02a13cbc41
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-06-10 23:31:37 +0600

    added problem soution 14

---
+++ src/Day-3/Problem-14.ts
@@ -0,0 +1,26 @@
+// Problem 14: Flatten a Nested Array  [Medium]
+// Description: Write a function flattenArray(arr) that flattens one level of a nested array.
+// Example:
+// Input: [1, [2, 3], [4, 5]]  → Output: [1, 2, 3, 4, 5]
+// Hint: Use flat() or reduce() with concat().
+// const  flattenArray=(arr:any[])=>{
+//    return arr.flat(Infinity)
+// }
+
+//recursionuse
+const  flattenArray=(arr:any[])=>{
+   return arr.reduce((acc:number[ ],curr)=>{
+
+       if(Array.isArray(curr))
+       {
+          acc.push(...flattenArray(curr))      
+       }else{
+          acc.push(curr)
+       }
+
+       return acc
+   },[])
+}
+
+
+console.log(flattenArray([1, [2, 3], [4, 5]]))
