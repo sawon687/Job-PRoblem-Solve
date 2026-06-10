18bb9f4891fb7d2296b486cdf248780577786216
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-06-10 23:10:32 +0600

    added problem soution 12

---
+++ src/Day-3/Problem-13.ts
@@ -0,0 +1,19 @@
+// Problem 13: Remove Duplicates from Array  [Easy]
+// Description: Write a function removeDuplicates(arr) that returns a new array with duplicate values removed.
+// Example:
+// Input: [1, 2, 2, 3, 3, 4]  → Output: [1, 2, 3, 4]
+// Hint: Use Set or filter() with indexOf().
+
+// const removeDuplicates=(arr:number[])=>{
+
+
+// return [...new Set(arr)]
+// }
+
+const removeDuplicates=(arr:number[])=>{
+
+  return arr.filter((item,index)=>
+    arr.indexOf(item)===index)
+
+}
+ console.log(removeDuplicates([1, 2, 2, 3, 3, 4] ))
