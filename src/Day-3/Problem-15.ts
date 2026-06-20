9f19cba59ba6dd371e6d713d514d6405dfedc8ec
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-06-20 22:43:48 +0600

    added update problem015

--- src/Day-3/Problem-15.ts
+++ src/Day-3/Problem-15.ts
@@ -4,17 +4,58 @@
 // Input: [1,2,3,4,5], 2  → Output: [[1,2],[3,4],[5]]
 // Hint: Use a while loop with slice().
 
-const chunkArray = (arr: number[], size: number) => {
-  const result: number[][] = [];
+ const chunkArray=(arr:number[],size:number)=>{
+       const result:number[][]=[]
+       let i=0;
+       while(i<arr.length)
+       {
+          result.push(arr.splice(i,size+i))
+          console.log('sawon',arr.splice(i,size+i))
+          i+size
+       }
 
-  let i = 0;
+       return [...result]
+ }
 
-  while (i < arr.length) {
-    result.push(arr.slice(i, i + size));
-    i += size;
-  }
+ console.log(chunkArray([1,2,3,4,5], 2 ))
 
-  return result;
-};
 
-console.log(chunkArray([1,2,3,4,5], 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const chunkArray = (arr: number[], size: number) => {
+//   const result: number[][] = [];
+
+//   let i = 0;
+
+//   while (i < arr.length) {
+//     result.push(arr.slice(i, i + size));
+//     i += size;
+//   }
+
+//   return result;
+// };
+
+// console.log(chunkArray([1,2,3,4,5], 2))
